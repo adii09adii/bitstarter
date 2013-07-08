b565bcf7de111719372b04dce19912230f67ca63
Aditi Agarwal <adii09adii@gmail.com>
2013-07-08 21:31:44 +0000

    try  web.js to display index file content

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-var indexcontent=fs.readFile('../index.html');
+var indexcontent=fs.readFile('index.html');
 buf = new Buffer(indexcontent.length);
 
 for (var i = 0; i < indexcontent.length ; i++) {
