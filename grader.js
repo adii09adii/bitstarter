1cf086121d5b2bee2fc3ed00ff3948d63125a3d7
Aditi Agarwal <adii09adii@gmail.com>
2013-07-11 22:16:15 +0000

    checked for both type

--- grader.js
+++ grader.js
@@ -27,9 +27,8 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 //var HTMLFILE_DEFAULT = "index.html";
 //var CHECKSFILE_DEFAULT = "checks.json";
-//var URLPATH_DEFAULT = "http://guarded-lowlands-2137.herokuapp.com/";
-//var inurl=false;
-
+	//var URLPATH_DEFAULT = "http://guarded-lowlands-2137.herokuapp.com/";
+var flag='0';
 /*
 var urlPass = function(inurl)
 {
@@ -71,18 +70,40 @@ var instr = infile.toString();
   //  return instr;
 
 //}; 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(htmlfile);
-//return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfile, flag) {
+//console.log("value of flag"+ flag);
+if (flag=='1')
+{return cheerio.load(fs.readFileSync(htmlfile));
+}else
+{//console.log(flag);
+	// if(flag=='2');
+	return cheerio.load(htmlfile);
+	//else
+	//{console.log("please input either url or file to check from json")};
+}
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-//var htmlfile = getHtmlFile(URLPATH_DEFAULT);   
- $ = cheerioHtmlFile(htmlfile);
+var getFile = function(infile) {
+var flag = '1';
+console.log(flag);
+var filename = infile.toString();
+    if(!fs.existsSync(filename)) {
+        console.log("%s does not exist. Exiting.", filename);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    var checkJson = checkHtmlFile(filename, program.checks, flag); 
+
+}
+var checkHtmlFile = function(htmlfile, checksfile, flag) {
+//var htmlfile = getHtmlFile(URLPATH_DEFAULT);
+//console.log(htmlfile+"html");
+//console.log(program.checks+"check");
+var htmlfile = htmlfile.toString();   
+ $ = cheerioHtmlFile(htmlfile, flag);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -101,6 +122,7 @@ var clone = function(fn) {
 };
 
 var getHtmlFile =  function(URLPATH_DEFAULT) {    
+var flag = '2';
     rest.get(URLPATH_DEFAULT).on('complete', function(result){
 	if (result instanceof Error) {        
         	// It's bad    
@@ -108,7 +130,7 @@ var getHtmlFile =  function(URLPATH_DEFAULT) {
    		 this.retry(5000);
     	} else {
 
-  var checkJson = checkHtmlFile(result, program.checks);
+  var checkJson = checkHtmlFile(result, program.checks, flag);
   // var outJson = JSON.stringify(checkJson, null, 4);
 //console.log(outJson);
 //		return "tmpIndex.html";
@@ -123,7 +145,7 @@ var getHtmlFile =  function(URLPATH_DEFAULT) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists))
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-f, --file <html_file>', 'path to html file', clone(getFile))
       .option('-u,  --url  <url>',       'url to crowdfundersite',clone(getHtmlFile))
  .parse(process.argv);
 //	console.log("getting remote file");
@@ -137,7 +159,7 @@ if(require.main == module) {
    //else
    //{  console.log('I am in file');
    
-    var checkJson = checkHtmlFile(program.file, program.checks);
+   // var checkJson = checkHtmlFile(program.file, program.checks);
    // var outJson = JSON.stringify(checkJson, null, 4);
     //console.log(outJson);
     //}
@@ -145,4 +167,3 @@ if(require.main == module) {
 else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
